83f34ac9c163ad4192149112ca70a36f9b25eac9
GST. NOUVAL WAHIDI <nouvalwahidi00@gmail.com>
2026-07-04 17:37:59 +0700

    Perubahan pada Model

--- src/model/Putusan.java
+++ src/model/Putusan.java
@@ -1,10 +1,12 @@
 package model;
 
-public class Putusan {
+public class Putusan extends DukomenPengadilan {
+    private String namaTerdakwa;
+    private int umurTerdakwa;
+    private String jenisNarkotika;
     private String nomorPerkara;
     private String pengadilan;
     private String tanggalPutusan;
-    private int umurTerdakwa;
     private double beratBarangBukti;
     private String pasalDilanggar;
     private String peranTerdakwa;
@@ -12,36 +14,157 @@ public class Putusan {
     private double vonisDenda;
     private String namaHakim;
 
-    // Constructor Parameterized (WAJIB ADA sesuai PDF)
-    public Putusan(String nomorPerkara, String pengadilan, String tanggalPutusan,
-                   String namaTerdakwa, int umurTerdakwa, String jenisNarkotika,
-                   double beratBarangBukti, String pasalDilanggar, String peranTerdakwa,
-                   int vonisHukuman, double vonisDenda, String namaHakim) {
-        this.nomorPerkara = nomorPerkara;
-        this.pengadilan = pengadilan;
-        this.tanggalPutusan = tanggalPutusan;
-        this.umurTerdakwa = umurTerdakwa;
-        this.beratBarangBukti = beratBarangBukti;
-        this. pasalDilanggar = pasalDilanggar;
-        this.peranTerdakwa = peranTerdakwa;
-        this.vonisHukuman = vonisHukuman;
-        this.vonisDenda = vonisDenda;
-        this.namaHakim = namaHakim;
+    private static int jumlahDibuat=0;
+
+    public Putusan() {
+        super("Belum Ada","Belum Ada","Belum Ada");
+        this.namaTerdakwa="Belum Ada";
+        this.jenisNarkotika="Belum Ada";
+        this.pasalDilanggar="Belum Ada";
+        this.peranTerdakwa="Belum Ada";
+        this.namaHakim="Belum Ada";
+        jumlahDibuat++;
     }
 
-    public int getVonisHukuman() {
+    public Putusan(String nomorPerkara,String pengadilan,String tanggalPutusan,String namaTerdakwa,int umurTerdakwa,String jenisNarkotika,double beratBarangBukti,String pasalDilanggar,String peranTerdakwa,int vonisHukuman,double vonisDenda,String namaHakim) {
+        super(nomorPerkara,pengadilan,tanggalPutusan);
+        this.namaTerdakwa=namaTerdakwa;
+        this.umurTerdakwa=umurTerdakwa;
+        this.jenisNarkotika=jenisNarkotika;
+        this.beratBarangBukti=beratBarangBukti;
+        this.pasalDilanggar=pasalDilanggar;
+        this.peranTerdakwa=peranTerdakwa;
+        this.vonisHukuman=vonisHukuman;
+        this.vonisDenda=vonisDenda;
+        this.namaHakim=namaHakim;
+        jumlahDibuat++;
+    }
+
+    public static int getJumlahDibuat() {
+        return jumlahDibuat;
+    }
+
+    @Override
+    public void tampilkan() {
+        System.out.println("["+nomorPerkara+"] Terdakwa: "+namaTerdakwa+" | Vonis: "+vonisHukuman+" Bulan");
+    }
+
+    public void tampilkan(boolean detail) {
+        if (detail){
+            System.out.println("======= DETAIL PUTUSAN NARKOTIKA =======");
+            System.out.println("Nomor Perkara : "+nomorPerkara);
+            System.out.println("Pengadilan    : "+pengadilan);
+            System.out.println("Tanggal       : "+tanggalPutusan);
+            System.out.println("Terdakwa      : "+namaTerdakwa+" ("+umurTerdakwa+" Tahun)");
+            System.out.println("Barang Bukti  : "+jenisNarkotika+" ("+beratBarangBukti+" gram)");
+            System.out.println("Pasal / Peran : "+pasalDilanggar+" / "+peranTerdakwa);
+            System.out.println("Vonis Hukum   : "+vonisHukuman+" Bulan Penjara");
+            System.out.println("Vonis Denda   : Rp "+vonisDenda);
+            System.out.println("Hakim Ketua   : "+namaHakim);
+            System.out.println("Kategori Kasus: "+getKategoriHukuman());
+            System.out.println("========================================");
+        } else {
+            tampilkan();
+        }
+    }
+
+    @Override
+    public String getKategoriHukuman() {
+        if (vonisHukuman<48) {
+            return "Ringan";
+        } else if (vonisHukuman<=120) {
+            return "Sedang";
+        } else {
+            return "Berat";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nomorPerkara+" | "+namaTerdakwa+" | "+jenisNarkotika+" | "+vonisHukuman+" Bulan";
+    }
+
+    public String getNomorPerkara(){
+        return nomorPerkara;
+    }
+    public void setNomorPerkara(String nomorPerkara) {
+        this.nomorPerkara = nomorPerkara;
+    }
+
+    public String getPengadilan(){
+        return pengadilan;
+    }
+    public void setPengadilan(String pengadilan) {
+        this.pengadilan = pengadilan;
+    }
+
+    public String getTanggalPutusan(){
+        return tanggalPutusan;
+    }
+    public void setTanggalPutusan(String tanggalPutusan) {
+        this.tanggalPutusan = tanggalPutusan;
+    }
+
+    public String getNamaTerdakwa(){
+        return namaTerdakwa;
+    }
+    public void setNamaTerdakwa(String namaTerdakwa) {
+        this.namaTerdakwa = namaTerdakwa;
+    }
+
+    public int getUmurTerdakwa(){
+        return umurTerdakwa;
+    }
+    public void setUmurTerdakwa(int umurTerdakwa) {
+        this.umurTerdakwa = umurTerdakwa;
+    }
+
+    public String getJenisNarkotika(){
+        return jenisNarkotika;
+    }
+    public void setJenisNarkotika(String jenisNarkotika) {
+        this.jenisNarkotika = jenisNarkotika;
+    }
+
+    public double getBeratBarangBukti(){
+        return beratBarangBukti;
+    }
+    public void setBeratBarangBukti(double beratBarangBukti) {
+        this.beratBarangBukti = beratBarangBukti;
+    }
+
+    public String getPasalDilanggar(){
+        return pasalDilanggar;
+    }
+    public void setPasalDilanggar(String pasalDilanggar) {
+        this.pasalDilanggar = pasalDilanggar;
+    }
+
+    public String getPeranTerdakwa(){
+        return peranTerdakwa;
+    }
+    public void setPeranTerdakwa(String peranTerdakwa) {
+        this.peranTerdakwa = peranTerdakwa;
+    }
+
+    public int getVonisHukuman(){
         return vonisHukuman;
     }
-
-    public void setVonisHukuman(int vonisHukuman){
+    public void setVonisHukuman(int vonisHukuman) {
         this.vonisHukuman = vonisHukuman;
     }
 
-    public double getVonisDenda() {
+    public double getVonisDenda(){
         return vonisDenda;
     }
-
-    public void setVonisDenda(int VonisDenda){
+    public void setVonisDenda(double vonisDenda) {
         this.vonisDenda = vonisDenda;
+    }
+
+    public String getNamaHakim(){
+        return namaHakim;
+    }
+    public void setNamaHakim(String namaHakim) {
+        this.namaHakim = namaHakim;
     }
 }
--- src/model/StatistikPutusan.java
+++ src/model/StatistikPutusan.java
@@ -1,9 +1,92 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class StatistikPutusan {
-    // Skeleton constructor
-    public StatistikPutusan(ArrayList<Putusan> daftar) {
+public class StatistikPutusan{
+    private int totalPutusan;
+    private double rataRataVonis;
+    private double rataRataDenda;
+    private String jenisNarkotikaTerbanyak;
+    private String distribusiPeran;
+
+    private ArrayList<Putusan>data;
+
+    public StatistikPutusan(ArrayList<Putusan>daftar){
+        this.data=daftar;
+        hitungSemua();
+    }
+
+    public void hitungSemua(){
+        this.totalPutusan=data.size();
+        if (totalPutusan==0){
+            this.rataRataVonis=0;
+            this.rataRataDenda=0;
+            this.jenisNarkotikaTerbanyak="Tidak ada data";
+            this.distribusiPeran="Tidak ada data";
+            return;
+        }
+
+        double totalVonis=0;
+        double totalDenda=0;
+        HashMap<String,Integer>petaNarkotika=new HashMap<>();
+        HashMap<String,Integer>petaPeran=new HashMap<>();
+
+        for (Putusan p:data){
+            totalVonis+=p.getVonisHukuman();
+            totalDenda+=p.getVonisDenda();
+
+            String jenis=p.getJenisNarkotika();
+            petaNarkotika.put(jenis,petaNarkotika.getOrDefault(jenis,0)+1);
+
+            String peran=p.getPeranTerdakwa();
+            petaPeran.put(peran,petaPeran.getOrDefault(peran,0)+1);
+        }
+
+        this.rataRataVonis=totalVonis/totalPutusan;
+        this.rataRataDenda=totalDenda/totalPutusan;
+
+        String maksNarkotika="";
+        int hitungMaksNarkotika=-1;
+        for (Map.Entry<String,Integer>entry:petaNarkotika.entrySet()){
+            if (entry.getValue()>hitungMaksNarkotika){
+                hitungMaksNarkotika=entry.getValue();
+                maksNarkotika=entry.getKey();
+            }
+        }
+        this.jenisNarkotikaTerbanyak=maksNarkotika+" ("+hitungMaksNarkotika+" kasus)";
+
+        StringBuilder sbPeran=new StringBuilder();
+        for (Map.Entry<String,Integer>entry:petaPeran.entrySet()){
+            sbPeran.append(entry.getKey()).append(": ").append(entry.getValue()).append(" | ");
+        }
+        this.distribusiPeran=sbPeran.toString();
+    }
+
+    public void tampilkanLaporan(){
+        System.out.println("\n================= LAPORAN STATISTIK KMS =================");
+        System.out.println("Total Kasus Putusan       : " + totalPutusan);
+        System.out.printf("Rata-rata Hukuman Penjara : %.2f Bulan\n", rataRataVonis);
+        System.out.printf("Rata-rata Nilai Denda     : Rp %,.2f\n", rataRataDenda);
+        System.out.println("Jenis Narkotika Terbanyak : " + jenisNarkotikaTerbanyak);
+        System.out.println("Distribusi Peran Terdakwa : " + distribusiPeran);
+        System.out.println("=========================================================");
+    }
+
+    public int getTotalPutusan(){
+        return totalPutusan;
+    }
+    public double getRataRataVonis(){
+        return rataRataVonis;
+    }
+    public double getRataRataDenda(){
+        return rataRataDenda;
+    }
+    public String getJenisNarkotikaTerbanyak(){
+        return jenisNarkotikaTerbanyak;
+    }
+    public String getDistribusiPeran(){
+        return distribusiPeran;
     }
 }
